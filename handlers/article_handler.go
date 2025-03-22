ad8973da2ef6d8ccb5df3bc07ebfb7ff6940fb27
kawa0x5f <yuyux0910@gmail.com>
2025-03-22 15:56:49 +0900

    feat: 特定の記事を取得するAPIを作成

--- handlers/article_handler.go
+++ handlers/article_handler.go
@@ -4,15 +4,18 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"kawa_blog/database"
 	"kawa_blog/models"
+
+	"github.com/gorilla/mux"
 )
 
 // 記事の一覧を取得する処理
 func GetArticles(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT id, title, content, image_url, created_at updated_at FROM articles")
+		rows, err := db.Query("SELECT id, title, content, image_url, created_at, updated_at FROM articles")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -22,7 +25,7 @@ func GetArticles(db *sql.DB) http.HandlerFunc {
 		var articles []models.Article
 		for rows.Next() {
 			var article models.Article
-			err := rows.Scan(&article.ID, &article.Title, &article.Content, &article.ImageURL, &article.CreatedAt)
+			err := rows.Scan(&article.ID, &article.Title, &article.Content, &article.ImageURL, &article.CreatedAt, &article.UpdatedAt)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
@@ -32,6 +35,46 @@ func GetArticles(db *sql.DB) http.HandlerFunc {
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(articles)
+	}
+}
+
+// 記事を取得する処理
+func GetArticle(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// ルートパラメータを取得
+		vars := mux.Vars(r)
+		idStr := vars["id"]
+
+		// int に変換
+		target_id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid article ID", http.StatusBadRequest)
+			return
+		}
+		rows, err := db.Query("SELECT id, title, content, image_url, created_at, updated_at FROM articles")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var article models.Article
+		for rows.Next() {
+			var row models.Article
+			err := rows.Scan(&row.ID, &row.Title, &row.Content, &row.ImageURL, &row.CreatedAt, &row.UpdatedAt)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if row.ID == target_id {
+				article = row
+				break
+			}
+		}
+
+		w.Header().Set("Cointent-Type", "application/json")
+		json.NewEncoder(w).Encode(article)
 	}
 }
 
--- routes/router.go
+++ routes/router.go
@@ -11,6 +11,7 @@ func NewRouter(db *sql.DB) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/articles", handlers.GetArticles(db)).Methods("GET")
 	r.HandleFunc("/articles", handlers.CreateArticle(db)).Methods("POST")
+	r.HandleFunc("/article/{id}", handlers.GetArticle(db)).Methods("GET")
 	r.HandleFunc("/tags", handlers.GetTags(db)).Methods("GET")
 	r.HandleFunc("/tags", handlers.CreateTag(db)).Methods("POST")
 	return r
