ac48cb746739d3e7b81a39680acfd0cf8356b765
kawa0x5f <yuyux0910@gmail.com>
2025-03-23 00:28:40 +0900

    fix: DeleteであるべきところがGetになっていたのを修正

--- routes/router.go
+++ routes/router.go
@@ -12,7 +12,7 @@ func NewRouter(db *sql.DB) *mux.Router {
 	r.HandleFunc("/articles", handlers.GetArticles(db)).Methods("GET")
 	r.HandleFunc("/articles", handlers.CreateArticle(db)).Methods("POST")
 	r.HandleFunc("/article/{slug}", handlers.GetArticle(db)).Methods("GET")
-	r.HandleFunc("/article/{slug}", handlers.GetArticle(db)).Methods("DELETE")
+	r.HandleFunc("/article/{slug}", handlers.DeleteArticle(db)).Methods("DELETE")
 	r.HandleFunc("/tags", handlers.GetTags(db)).Methods("GET")
 	r.HandleFunc("/tags", handlers.CreateTag(db)).Methods("POST")
 	return r
