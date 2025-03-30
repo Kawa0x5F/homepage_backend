1d480fd683a1c077f1db1a5637995ddf6fc7daf8
kawa0x5f <yuyux0910@gmail.com>
2025-03-30 17:19:07 +0900

    feat: SkillsとContactに関してDBを操作する処理を追加

--- database/contact.go
+++ database/contact.go
@@ -1,1 +1,73 @@
 package database
+
+import (
+	"database/sql"
+	"kawa_blog/models"
+	"log"
+)
+
+// 全データを取得するコード
+func GetAllContact(db *sql.DB) ([]models.Contact, error) {
+	query := `
+	SELECT id, name, link, has_image, created_at
+	FROM contact
+	`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var contacts []models.Contact
+
+	for rows.Next() {
+		var contact models.Contact
+		err := rows.Scan(&contact.ID, &contact.Name, &contact.Link, &contact.HasImage, &contact.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		contacts = append(contacts, contact)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return contacts, nil
+}
+
+// DBにデータを追加する
+func InsertContact(db *sql.DB, contact *models.Contact) error {
+	query := `
+	INSERT INTO contact (name, link, has_image, created_at)
+	VALUES($1, $2, $3, NOW()) RETURNING id, created_at
+	`
+
+	err := db.QueryRow(query, contact.Name, contact.Link, contact.HasImage).Scan(&contact.ID, &contact.CreatedAt)
+	if err != nil {
+		log.Println("Contactの挿入に失敗:", err)
+		return err
+	}
+	return nil
+}
+
+// DBからデータを削除する
+func DeleteContactByID(db *sql.DB, id int) (bool, error) {
+	result, err := db.Exec("DELETE FROM contact WHERE id = $1", id)
+	if err != nil {
+		return false, err
+	}
+
+	// 削除された行数を確認
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if rowsAffected == 0 {
+		return false, err
+	}
+
+	return true, nil
+}
--- database/skills.go
+++ database/skills.go
@@ -1,1 +1,73 @@
 package database
+
+import (
+	"database/sql"
+	"kawa_blog/models"
+	"log"
+)
+
+// 全データを取得するコード
+func GetAllSkills(db *sql.DB) ([]models.Skills, error) {
+	query := `
+	SELECT id, name, type, has_image, created_at
+	FROM skills
+	`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var skills []models.Skills
+
+	for rows.Next() {
+		var skill models.Skills
+		err := rows.Scan(&skill.ID, &skill.Name, &skill.Type, &skill.HasImage, &skill.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		skills = append(skills, skill)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return skills, nil
+}
+
+// DBにデータを追加する
+func InsertSkills(db *sql.DB, skills *models.Skills) error {
+	query := `
+	INSERT INTO skills (name, type, has_image, created_at)
+	VALUES($1, $2, $3, NOW()) RETURNING id, created_at
+	`
+
+	err := db.QueryRow(query, skills.Name, skills.Type, skills.HasImage).Scan(&skills.ID, &skills.CreatedAt)
+	if err != nil {
+		log.Println("Skillの挿入に失敗:", err)
+		return err
+	}
+	return nil
+}
+
+// DBからデータを削除する
+func DeleteSkillByID(db *sql.DB, id int) (bool, error) {
+	result, err := db.Exec("DELETE FROM skills WHERE id = $1", id)
+	if err != nil {
+		return false, err
+	}
+
+	// 削除された行数を確認
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if rowsAffected == 0 {
+		return false, err
+	}
+
+	return true, nil
+}
